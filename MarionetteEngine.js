a93862a75bc37fc4b5203204b39d1933e031016d
hong <ghhong@wemb.co.kr>
2021-05-11 15:53:42 +0900

    필요없는 코드 삭제
    ㅇㅇ

--- MarionetteEngine.js
+++ MarionetteEngine.js
@@ -10,13 +10,7 @@ const chalk = require('chalk');
 const {actionScraper} = require('./Scraper/actionScraper')
 const eventBus = require('js-event-bus')();
 
-exports.isCanceled = {
-  flag : false
-};
-  
 exports.eb = eventBus;
-
-
 /**
  * execute list of routines. *
  * @param {Routine[]} routines - The routine include of action information* 
@@ -41,24 +35,8 @@ exports.MarionetteEngine = async (routines) => {
     eventBus.emit('cancel', null, 'bot1');
   }, 10000)
 
-  eventBus.on('cancelWorker', async (cancelBotName, workerId, deleteInf) => {  
-    // console.log(cluster.workers.length)
-    // console.log(workerId)     
-    // console.log(cluster.workers[workerId])    
-    // cluster.workers.splice(workerId, 1);
-    // cluster.jobQueue.list.splice(workerId, 1);
-    // console.log(cluster.jobQueue.list);
-    // console.log(cluster.workers.length);
-    // const a = cluster.workersBusy.map((w, i)=> w.id === workerId ? i : null).filter((d)=> d !== null);
-    // if(a.length){            
-    //   cluster.workers[a[0]]
-    // }
-    // console.log(cluster.workersBusy)
-    // console.log(cluster.workersAvail)
-    console.log(chalk.redBright(cancelBotName + ' 봇 워커 종료'))   
-    this.isCanceled.flag = true;
-    // eventBus.emit('cancelTask', null, cancelBotName)
-    //삭제 정보를 넘겨 받고 .. 처리하는 곳
+  eventBus.on('cancelWorker', async (cancelBotName, workerId, deleteInf) => {      
+    console.log(chalk.redBright(cancelBotName + ' 봇 워커 종료'))       
   })
 
   eventBus.on('error', (errType, message) => {
